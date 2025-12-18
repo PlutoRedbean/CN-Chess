61bccf399e52d5a1a2e61560e2567297f2ffe037
PlutoRedbean <HD3197388203@outlook.com>
2025-12-18 14:06:48 +0800

    feat: Advisor movement

--- src/Piece/Advisor.java
+++ src/Piece/Advisor.java
@@ -12,9 +12,22 @@ public class Advisor extends Piece {
         if (side == Piece.RED) return "仕";
         else return "士";
     }
+    
+    private boolean isDiagonal(int targetRow, int targetCol) {
+        return Math.abs(targetRow - getRow()) == 1 && Math.abs(targetCol - getCol()) == 1;
+    }
+
+    private boolean isInPalace(int targetRow, int targetCol) {
+        if (targetCol < 3 || targetCol > 5) return false;
+        if (side == Piece.RED) {
+            return targetRow >= 7 && targetRow <= 9;
+        } else {
+            return targetRow >= 0 && targetRow <= 2;
+        }
+    }
 
     @Override
     public boolean isValidMove(int targetRow, int targetCol, Board board) {
-        return true;
+        return isDiagonal(targetRow, targetCol) && isInPalace(targetRow, targetCol);
     }
 }
